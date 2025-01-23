e3f6718e3fea7191a711c7621b1e8fa4786a4672
Ilya <ilya02989@gmail.com>
2025-01-23 10:36:13 +0300

    init

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Academy</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="header">
+                <div class="header__logo">
+                    <img src="img/logo.png" alt="logo">
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="">О школе</a></li>
+                        <li><a href="">Группы</a></li>
+                        <li><a href="">Тренеры</a></li>
+                        <li><a href="">Расписание</a></li>
+                        <li><a href="">Контакты</a></li>
+                    </ul>
+                </nav>
+                <button id="modal-button">Вход/Регистрация</button>
+            </div>
+
+        </div>
+    </header>
+
+    <main>
+        <section class="hero">
+            <div class="hero__content">
+                <h1>Детская академия <br> футбола валерия <br> кечинова</h1>
+                <p>Ведется набор от 5 до 15 лет.</p>
+                <p>Пробная тренировка бесплатно.</p>
+                <button>Записаться</button>
+            </div>
+        </section>
+
+        <section class="about">
+
+            <div class="about__features">
+                <div class="about__card">
+                    <img src="img/football_ball.svg" alt="изображение мяча">
+                    <div class="about__card_text">
+                        <h3>Тренерский состав</h3>
+                        <p>
+                            Квалифицированный тренерский штаб нашей футбольной школы имеет не только спортивное, но и
+                            педагогическое образование</p>
+                    </div>
+                </div>
+                <div class="about__card">
+                    <img src="img/football_camo.svg" alt="иконка лагеря">
+                    <div class="about__card_text">
+                        <h3>Футбольные сборы и лагеря</h3>
+                        <p>
+                            Школа футбола «Академия спорта» регулярно проводит клубные футбольные сборы .Также работает
+                            летний лагерь</p>
+                    </div>
+                </div>
+                <div class="about__card">
+                    <img src="img/journall.svg" alt="иконка журнала">
+                    <div class="about__card_text">
+                        <h3>Индивидуальные тренировки</h3>
+                        <p>
+                            Специалисты нашей детской школы футбола с удовольствием помогут каждому улучшить технические
+                            навыки индивидуально</p>
+                    </div>
+                </div>
+            </div>
+
+            <h2 class="about__title">О школе</h2>
+            <p class="about__text">Футбол которому обучает академия основывается на двух правилах: своевременный умный дриблинг, точный и
+                удобный пас.</p>
+            <div class="about__info">
+                <img src="img/about_image.jpg" alt="about">
+                <div class="about__info_text">
+                    <p>В тренировочном процессе футболистов используются самые разнообразные средства и методы, с
+                        помощью которых совершенствуются основные физические качества и технико-тактическое мастерство
+                        спортсменов:</p>
+                        <ul>
+                            <li>-развитие точности удара;</li>
+                            <li>-развитие быстроты;</li>
+                            <li>-развитие силы и скоростно-силовых качеств;</li>
+                            <li>-развитие общей выносливости.</li>
+                        </ul>
+                </div>
+            </div>
+        </section>
+
+
+    </main>
+</body>
+
+</html>
